e9b344804b6630a41287185839ebee6bd5ec88d1
yoko8ma <hideyuki.yokoyama@gmail.com>
2020-04-26 09:39:25 +0900

    27

---
+++ steps/step27.py
@@ -0,0 +1,53 @@
+import numpy as np
+from dezero import Variable, Function
+from dezero.utils import plot_dot_graph
+
+
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = gy * np.cos(x)
+        return gx
+
+
+def sin(x):
+    return Sin()(x)
+
+
+x = Variable(np.array(np.pi/4))
+y = sin(x)
+y.backward()
+
+print(y.data)
+print(x.grad)
+
+
+import math
+
+
+def my_sin(x, threshold=0.0001):
+    y = 0
+    for i in range(100000):
+        c = (-1) ** i / math.factorial(2 * i + 1)
+        t = c * x ** (2 * i + 1)
+        y = y + t
+        if abs(t.data) < threshold:
+            break
+
+    return y
+
+
+x = Variable(np.array(np.pi/4))
+y = my_sin(x)
+y.backward()
+
+print(y.data)
+print(x.grad)
+
+x.name = 'x'
+y.name = 'y'
+plot_dot_graph(y, verbose=False, to_file='sin.png')
